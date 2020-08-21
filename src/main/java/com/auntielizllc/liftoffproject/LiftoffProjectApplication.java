bae6e109fc6f88d6a370b5d5e655d6b2d8ab050c
Nolan <nolanluhm@gmail.com>
2020-08-21 00:38:20 -0500

    Initialized project with a Hello, Spring page

---
+++ src/main/java/com/auntielizllc/liftoffproject/LiftoffProjectApplication.java
@@ -0,0 +1,13 @@
+package com.auntielizllc.liftoffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftoffProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftoffProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auntielizllc/liftoffproject/LiftoffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auntielizllc.liftoffproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
