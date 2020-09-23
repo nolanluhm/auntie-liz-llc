c73a34c865886fd5b3bc93df5e34c8f5e1413811
Nolan <nolan@ADM-122-201617.local>
2020-09-23 11:22:53 -0500

    Added setHost method to fix permissions issues with localhost for testing.

--- src/main/java/CalendarQuickstart.java
+++ src/main/java/CalendarQuickstart.java
@@ -54,7 +54,7 @@ public class CalendarQuickstart {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).setHost("domainhere.com").build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
