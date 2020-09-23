edc1c2d14c52db427ebbc5f562cc8669b2def7f6
cluhm <cluhm@expediagroup.com>
2020-09-22 22:15:56 -0500

    oauth_example - Basic calendar access

--- src/main/java/com/auntielizllc/liftoffproject/LiftoffProjectApplication.java
+++ src/main/java/com/auntielizllc/liftoffproject/LiftoffProjectApplication.java
@@ -1,5 +1,7 @@
 package com.auntielizllc.liftoffproject;
 
+import com.auntielizllc.liftoffproject.deleteme.CalendarQuickstart;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiftoffProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LiftoffProjectApplication.class, args);
+		SpringApplication.run(CalendarQuickstart.class, args);
 	}
 
 }
