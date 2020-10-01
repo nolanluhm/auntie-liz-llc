a6574fa37d6bd3cade3ca51f29d2ed731b3c1c45
Nolan <nolanluhm@gmail.com>
2020-10-01 17:51:23 -0500

    Added displayNewClientFormPage function.

--- src/main/java/com/auntielizllc/liftoffproject/controllers/BusinessInformationController.java
+++ src/main/java/com/auntielizllc/liftoffproject/controllers/BusinessInformationController.java
@@ -1,14 +1,13 @@
 package com.auntielizllc.liftoffproject.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BusinessInformationController {
 
     @GetMapping
-    public String displayHomePage(Model model) {
+    public String displayHomePage() {
         return "index";
     }
 
