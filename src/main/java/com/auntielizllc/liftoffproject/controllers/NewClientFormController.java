dc6c15437d936662a9a98994ea8382c13f5134ce
Nolan <nolanluhm@gmail.com>
2020-10-03 15:33:38 -0500

    Fixed div tags and changed footer classes to center the business title in the middle of the footer container.

--- src/main/java/com/auntielizllc/liftoffproject/controllers/NewClientFormController.java
+++ src/main/java/com/auntielizllc/liftoffproject/controllers/NewClientFormController.java
@@ -1,8 +1,7 @@
 package com.auntielizllc.liftoffproject.controllers;
 
-import com.auntielizllc.liftoffproject.data.NewClientRepository;
-import com.auntielizllc.liftoffproject.models.NewClient;
-import com.auntielizllc.liftoffproject.models.dto.LoginFormDTO;
+import com.auntielizllc.liftoffproject.data.CustomerRepository;
+import com.auntielizllc.liftoffproject.models.Customer;
 import com.auntielizllc.liftoffproject.models.dto.NewClientFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,23 +10,25 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
-public class NewClientController {
+@RequestMapping("schedule-appointment/new-client-form")
+public class CustomerController {
 
     @Autowired
-    NewClientRepository newClientRepository;
+    CustomerRepository customerRepository;
 
-    @GetMapping("schedule-appointment/new-client-form")
+    @GetMapping
     public String displayNewClientFormPage(Model model) {
         model.addAttribute(new NewClientFormDTO());
         return "new_client_form";
     }
 
-    @PostMapping("/schedule-appointment/new-client-form")
+    @PostMapping
     public String processNewClientForm(@ModelAttribute @Valid NewClientFormDTO newClientFormDTO,
                                        Errors errors, HttpServletRequest request,
                                        Model model) {
@@ -36,8 +37,8 @@ public class NewClientController {
             return "new_client_form";
         }
 
-        NewClient newClient = new NewClient(newClientFormDTO.getFirstName(), newClientFormDTO.getLastName());
-        newClientRepository.save(newClient);
+        Customer newCustomer = new Customer(newClientFormDTO.getFirstName(), newClientFormDTO.getLastName());
+        customerRepository.save(newCustomer);
 
         return "redirect:"; 
     }
