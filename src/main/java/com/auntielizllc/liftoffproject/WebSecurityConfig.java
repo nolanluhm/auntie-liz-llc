ffb53fa43805167a53bcd54018aee980076777b4
nolanluhm <nolanluhm@gmail.com>
2020-10-15 11:26:34 -0500

    Patched a bug that gave a 403 error on new client form submission.

--- src/main/java/com/auntielizllc/liftoffproject/WebSecurityConfig.java
+++ src/main/java/com/auntielizllc/liftoffproject/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.auntielizllc.liftoffproject;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +12,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable();
+
         http.requiresChannel()
                 .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
                 .requiresSecure();
