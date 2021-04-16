ff1d799c50324a960db3d08946fdc2bd09679d7f
Nolan <nolanluhm@gmail.com>
2021-04-16 17:16:24 -0500

    Removed all code related to product orders.

--- src/main/java/com/auntielizllc/liftoffproject/data/ProductOrderRepository.java
+++ src/main/java/com/auntielizllc/liftoffproject/data/ProductOrderRepository.java
@@ -1,6 +1,5 @@
 package com.auntielizllc.liftoffproject.data;
 
-import com.auntielizllc.liftoffproject.models.ProductOrder;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductOrderRepository extends CrudRepository<ProductOrder, Integer> {}
