97dfb3d138f9d5d98627efd4c8f39d59a5d442aa
Nolan <nolanluhm@gmail.com>
2021-04-16 17:37:44 -0500

    Removed the ProductOrderRepository.

--- src/main/java/com/auntielizllc/liftoffproject/data/ProductOrderRepository.java
+++
@@ -1,5 +0,0 @@
-package com.auntielizllc.liftoffproject.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductOrderRepository extends CrudRepository<ProductOrder, Integer> {}
